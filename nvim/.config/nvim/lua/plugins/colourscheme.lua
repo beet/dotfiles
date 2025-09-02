3cf2398e91fe91590714485a9f89dc567ca78e0a
Mark Beattie <beattie.mark@gmail.com>
2025-09-02 20:47:15 +1000

    Checking in my NeoVim config in a nested subdirectory

---
+++ nvim/.config/nvim/lua/plugins/colourscheme.lua
@@ -0,0 +1,13 @@
+if true then
+  return {}
+end
+
+return {
+  { "Shatur/neovim-ayu" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "ayu-mirage",
+    },
+  },
+}
---
+++ nvim/.config/nvim/lua/plugins/test.vim.lua
@@ -0,0 +1,34 @@
+return {
+  "vim-test/vim-test",
+
+  -- keys = {
+  --   { "<leader>bb", ":TestLast<cr>", "Run last" },
+  --   { "<leader>bn", ":TestNearest<cr>", "Run nearest" },
+  --   { "<leader>be", ":TestFile<cr>", "Run file" },
+  --   { "<leader>ba", ":TestSuite<cr>", "Run all" },
+  -- }
+
+  -- config = function()
+  --   vim.g["test#custom_strategies"] = {
+  --     my_neovim = function(cmd)
+  --       -- Open a new vertical split for the terminal
+  --       vim.ux.open_on_right_side()
+  --
+  --       -- Run the test command
+  --       vim.fn.jobstart(cmd, {
+  --         term = true,
+  --       })
+  --
+  --       -- Set buffer-local variable so that terminal doesn't enter insert mode
+  --       local bufnr = vim.api.nvim_get_current_buf()
+  --       vim.api.nvim_buf_set_var(bufnr, "is_vimtest_terminal", true)
+  --
+  --       vim.cmd.wincmd("p") -- Go back to the previous window
+  --       vim.cmd.stopinsert() -- Make sure we are not in insert mode
+  --     end,
+  --   }
+  --
+  --   -- Use custom strategy
+  --   vim.g["test#strategy"] = "my_neovim"
+  -- end,
+}
