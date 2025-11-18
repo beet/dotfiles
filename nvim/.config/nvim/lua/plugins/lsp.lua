c7a2e6cb9ef01a8141a6dd6827a92f4c45783e23
Mark Beattie <mark.beattie@creditorwatch.com.au>
2025-11-18 16:32:33 +1100

    Trying to stop ruby-lsp spinning up multiple instances

---
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,13 @@
+local nvim_lsp = require("lspconfig")
+-- local cmp = require("cmp_nvim_lsp")
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+-- If they're not working, try running `gem install solargraph` and `gem install ruby-lsp`
+-- ruby-lsp requires ruby version 2.7.3 or higher and nvim 0.10.0
+nvim_lsp.ruby_lsp.setup({
+  cmd = { "/Users/markbeattie/.rbenv/shims/ruby-lsp" },
+  -- capabilities = cmp.default_capabilities(capabilities),
+})
+
+return {}
